a72ab49e610715ee7dc36219897a1dacc8d0edc0
sEjalBisen <sejalbisen12@gmail.com>
2026-05-09 20:18:43 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,36 +1,11 @@
-// Write your code here
-import MoviesSlider from '../MoviesSlider'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './App.css'
 
-import './index.css'
-
-const actionMovie = 'ACTION'
-const comedyMovie = 'COMEDY'
-
-const PrimeVideo = props => {
-  const {moviesList} = props
-  const actionMoviesList = moviesList.filter(
-    movie => movie.categoryId === actionMovie,
-  )
-
-  const comedyMoviesList = moviesList.filter(
-    movie => movie.categoryId === comedyMovie,
-  )
-
-  return (
-    <div className="prime-video-container">
-      <img
-        className="image"
-        src="https://assets.ccbp.in/frontend/react-js/prime-video-img.png"
-        alt="prime video"
-      />
-      <div className="movies-container">
-        <h1 className="movies-heading">Action Movies</h1>
-        <MoviesSlider moviesList={actionMoviesList} />
-        <h1 className="movies-heading">Comedy Movies</h1>
-        <MoviesSlider moviesList={comedyMoviesList} />
-      </div>
-    </div>
-  )
-}
-
-export default PrimeVideo
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+)
